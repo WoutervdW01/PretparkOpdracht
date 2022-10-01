be242ddd6001ec88c73b404846f24aeaa3de261d
Wouter van der Wekken <Wouter.van.der.Wekken@gmail.com>
2022-10-01 17:56:25 +0200

    Tabel gebruiker in sqlite db

---
+++ src/pretpark/database/Migrations/20221001155443_gebruiker_sqlite.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace database.Migrations
+{
+    public partial class gebruiker_sqlite : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "gebruikers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_gebruikers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "gebruikers");
+        }
+    }
+}
--- src/pretpark/database/Program.cs
+++ src/pretpark/database/Program.cs
@@ -1,7 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+class Gebruiker
+{
+    public int Id{get; set;}
+    public string Email{get; set;}
+}
+
 class DatabaseContext : DbContext
 {
+    public DbSet<Gebruiker> gebruikers {get; set;}
+
     protected override void OnModelCreating(ModelBuilder builer)
     {
 
@@ -9,7 +17,7 @@ class DatabaseContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
-
+        builder.UseSqlite("Data Source = database.db");
     }
 
     public static void Main(string[] args)
