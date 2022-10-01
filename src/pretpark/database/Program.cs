23ed3a5dfff16b3d0583dca220bcbf787824c37b
Wouter van der Wekken <Wouter.van.der.Wekken@gmail.com>
2022-10-01 13:41:21 +0200

    Eerste opzet Database connection

--- src/pretpark/database/Program.cs
+++ src/pretpark/database/Program.cs
@@ -2,9 +2,18 @@
 
 class DatabaseContext : DbContext
 {
+    protected override void OnModelCreating(ModelBuilder builer)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+
+    }
+
     public static void Main(string[] args)
     {
         
     }
 }
-
